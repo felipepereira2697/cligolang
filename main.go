72b54c8cb822a5e41d0a8eff3878ee7165c68489
felipepereira2697 <felipepereira2697@gmail.com>
2023-11-21 23:01:41 +0000

    100 Days of Code - Enhancing CLI with Cobra package

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	//Creating a basic cli with Golang
-	//To execute: go build .
-	//./cligolang -channelName YourStringVariableHere
+	var rootCmd = &cobra.Command{Use: "aryago"}
+	var name, pass string
+	var cmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create an user",
+		Run: func(cmd *cobra.Command, args []string) {
+			if name == "" {
+				fmt.Println("Sorry, name cannot be blank")
+				return
+			}
 
-	//First parameter: variable name, second: the default value, third the channel name
-	channel := flag.String("channelName", "Arya", "fp")
-	flag.Parse()
+			if len(pass) < 6 {
+				fmt.Println("Sorry, pass to weak, please provide a new pass")
+				return
+			}
 
-	os.Mkdir(fmt.Sprintf("%s", *channel), 077)
-	fmt.Println(*channel)
+			fmt.Printf("Name: %s\n Pass: %s\n", name, pass)
+		},
+	}
+
+	cmd.Flags().StringVarP(&name, "name", "n", "", "User name")
+	cmd.Flags().StringVarP(&pass, "pass", "p", "", "Password")
+
+	//Command sample: ./cligolang create -n Stark -p Stark1234
+	rootCmd.AddCommand(cmd)
+	rootCmd.Execute()
 
 }
